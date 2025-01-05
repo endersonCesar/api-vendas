06ad7ecda7613f90aa274a8b09f9d72ec967e928
enderson.brufatto <enderson.brufatto@grupocard.com.br>
2025-01-05 15:31:54 -0400

    nova rota

--- src/routes/estoque/estoque.route.js
+++ src/routes/estoque/estoque.route.js
@@ -6,7 +6,7 @@ const controller = require('../../controller/estoque/estoque.controller');
 
 
 router.post('/inserir', controller.cadastrarEstoque);
- router.post('/buscar', controller.buscarEstoque);
+router.post('/buscar', controller.buscarEstoque);
 router.get('/deletar/:id', controller.excluirEstoque);
 router.get('/buscarId/:id', controller.buscarId);
 router.put('/editar/:id', controller.editarEstoque);
